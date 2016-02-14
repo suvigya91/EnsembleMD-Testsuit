16da69e6c894cf5d14ded47555c95f153b8e772b
Suvigya Tripathi <suvigya91@gmail.com>
2016-02-14 18:55:23 -0500

    test jenkins

--- patterns/j_test_pattern.sh
+++ patterns/j_test_pattern.sh
@@ -3,5 +3,7 @@ PWD=$CWD"/patterns"
 RWD=$CWD"/reports"
 #CWD="/home/suvigya/radical.ensemblemd-master/tests/tests/patterns"
 
-py.test ./patterns/test_pipeline_api.py 
+py.test ./patterns/test_pipeline_api.py
+
+#hello world
 
