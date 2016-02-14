b582acc8f4749fe4a90f5fec1cb5d8d4be7196a2
Suvigya Tripathi <suvigya91@gmail.com>
2016-02-14 15:41:10 -0500

    [Test]

--- patterns/j_test_pattern.sh
+++ patterns/j_test_pattern.sh
@@ -3,5 +3,5 @@ PWD=$CWD"/patterns"
 RWD=$CWD"/reports"
 #CWD="/home/suvigya/radical.ensemblemd-master/tests/tests/patterns"
 
-py.test ./patterns/test_pipeline_api.py --junitxml=$RWD/pattern_api.xml --html=$RWD/pattern_api.html
+py.test ./patterns/test_pipeline_api.py 
 
