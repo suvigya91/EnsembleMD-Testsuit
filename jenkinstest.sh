4b48eb2d43892d4a6e0a777babc6df24b5c78b08
Suvigya Tripathi <suvigya91@gmail.com>
2016-02-14 14:44:15 -0500

    [Test] Add Jenkins test script

---
+++ jenkinstest.sh
@@ -0,0 +1,11 @@
+CWD=$HOME"/EnsembleMD\Testing"
+PWD=$CWD"/patterns"
+EXWD=$CWD"/execution_api"
+E2EWD=$CWD"/E2E_test"
+#echo $CWD
+
+
+sh $PWD"/tests_patterns.sh"
+#sh $EXWD"/executionapi_testcase.sh"
+#sh $E2EWD"/E2E_testcases.sh"
+
---
+++ patterns/j_test_pattern.sh
@@ -0,0 +1,7 @@
+CWD=$HOME"/EnsembleMD\Testing"
+PWD=$CWD"/patterns"
+RWD=$CWD"/reports"
+#CWD="/home/suvigya/radical.ensemblemd-master/tests/tests/patterns"
+
+py.test $PWD/test_*.py --junitxml=$RWD/pattern_api.xml --html=$RWD/pattern_api.html
+
