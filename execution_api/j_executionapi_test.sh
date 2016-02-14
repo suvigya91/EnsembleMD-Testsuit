90ff7c94ae3c91c0642f6dbb35896db7b015f190
Suvigya Tripathi <suvigya91@gmail.com>
2016-02-14 16:03:55 -0500

    Testing jenkins

--- execution_api/j_executionapi_test.sh
+++ execution_api/j_executionapi_test.sh
@@ -2,6 +2,6 @@ CWD=$HOME"/REnsembleMDTesting"
 EXWD=$CWD"/execution_api"
 RWD=$CWD"/reports"
 
-py.test $EXWD/test_*.py 
+py.test ./execution_api/test_*.py 
 #--junitxml=$RWD/execution_api.xml --html=$RWD/execution_api.html
 
