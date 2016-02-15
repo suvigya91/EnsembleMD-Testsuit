7b5e5b1b7d377a83fa7053efbe2b5f6f6564a535
Suvigya Tripathi <suvigya91@gmail.com>
2016-02-15 12:38:26 -0500

    test jenkins

--- patterns/j_test_pattern.sh
+++ patterns/j_test_pattern.sh
@@ -3,8 +3,9 @@ PWD=$CWD"/patterns"
 RWD=$CWD"/reports"
 #CWD="/home/suvigya/radical.ensemblemd-master/tests/tests/patterns"
 
+#py.test --cov-report xml --cov=. 
 py.test ./patterns/test_pipeline_api.py
 
 #EnsembleMD testing framework
-#MS Thesis
+#MS Thesis 2016
 
