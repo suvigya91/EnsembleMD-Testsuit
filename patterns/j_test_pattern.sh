a0a5c5f61fe9e442c59923dff622ac58f79e11dc
Suvigya Tripathi <suvigya91@gmail.com>
2016-02-16 16:05:25 -0500

    Added more test files to pattern script

--- patterns/j_test_pattern.sh
+++ patterns/j_test_pattern.sh
@@ -4,7 +4,7 @@ RWD=$CWD"/reports"
 #CWD="/home/suvigya/radical.ensemblemd-master/tests/tests/patterns"
 
 #py.test --cov-report xml --cov=. 
-py.test ./patterns/test_pipeline_api.py
+py.test ./patterns/test_*.py
 
 #EnsembleMD testing framework
 #MS Thesis May 2016
