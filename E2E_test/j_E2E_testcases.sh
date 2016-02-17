fd0f4c0304f4b906bab1a85bc18d2fa6a98dab2d
Suvigya Tripathi <suvigya91@gmail.com>
2016-02-16 21:04:25 -0500

    Updated test script for E2E

--- E2E_test/j_E2E_testcases.sh
+++ E2E_test/j_E2E_testcases.sh
@@ -5,8 +5,10 @@ RWD=$CWD"/reports"
 
 #py.test $E2EWD/test_*.py --cmdopt=xsede.gordon --junitxml=$RWD/e2e_remote_gordon.xml --html=$RWD/e2e_remote_gordon.html
 
-py.test ./E2E_tests/test_j_*.py --cmdopt=xsede.stampede #--junitxml=$RWD/e2e_remote_stampede.xml --html=$RWD/e2e_remote_stampede.html
-
+py.test ./E2E_tests/test_j_pipeline_E2E.py --cmdopt=xsede.stampede #--junitxml=$RWD/e2e_remote_stampede.xml --html=$RWD/e2e_remote_stampede.html
+py.test ./E2E_tests/test_j_allpairs_example.py --cmdopt=xsede.stampede
+py.test ./E2E_tests/test_j_replicaexchange_E2E.py --cmdopt=xsede.stampede
+py.test ./E2E_tests/test_j_simulation_analysis_loop.py --cmdopt=xsede.stampede
 
 
 
